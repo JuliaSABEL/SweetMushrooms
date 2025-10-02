a90077b474369dfeb2f50adcbf2d1326bb94cdaf
JuliaSABEL <sabelnikova.j.partner@gmail.com>
2025-10-02 17:45:01 +0300

    PlayerMover Added

--- Assets/SweetMushrooms/Scripts/Entities/Character/PlayerMover.cs
+++ Assets/SweetMushrooms/Scripts/Entities/Character/PlayerMover.cs
@@ -1,7 +1,34 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 
 public class PlayerMover : MonoBehaviour
 {
-    
+    [SerializeField] private PlayerSettings _playerSettings;
+    [SerializeField] private Rigidbody2D _rb;
+
+    private Vector2 _moveInput;
+    private InputSystem_Actions _inputSystem;
+
+
+    private void Awake() => _inputSystem = new InputSystem_Actions();
+
+    private void OnEnable()
+    {
+        _inputSystem.Player.Enable();
+        _inputSystem.Player.Move.performed += OnMovePerformed;
+        _inputSystem.Player.Move.canceled += OnMoveCanceled;
+    }
+
+    private void FixedUpdate() => _rb.linearVelocity = _moveInput * _playerSettings.speed;
+
+    private void OnDisable()
+    {
+        _inputSystem.Player.Move.canceled -= OnMoveCanceled;
+        _inputSystem.Player.Move.performed -= OnMovePerformed;
+        _inputSystem.Player.Disable();
+    }
+
+    private void OnMovePerformed(InputAction.CallbackContext context) => _moveInput = context.ReadValue<Vector2>();
+    private void OnMoveCanceled(InputAction.CallbackContext context) => _moveInput = Vector2.zero;
 }
