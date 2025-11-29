74659a4dd25e8c8121f9cffb0956d19de42049f0
JuliaSABEL <sabelnikova.j.partner@gmail.com>
2025-11-29 19:47:46 +0200

    Added TargetObjectController

--- Assets/SweetMushrooms/Scripts/Entities/TargetObjectController.cs
+++ Assets/SweetMushrooms/Scripts/Entities/TargetObjectController.cs
@@ -1,13 +1,15 @@
+using System;
 using UnityEngine;
 
 
 public class TargetObjectController : MonoBehaviour
 {
+    public event Action OnOwnerChanged;
+    
     public ICarrier CurrentOwner { get; private set; }
     
     [SerializeField] private TargetObjectSettings _targetObjectSettings;
     [SerializeField] private Rigidbody2D _rigidbody;
-    [SerializeField] private Collider2D _collider;
 
     private float _cooldownUntil;
 
@@ -15,14 +17,37 @@ public class TargetObjectController : MonoBehaviour
     public void TryCapture(ICarrier candidate)
     {
         if (Time.time < _cooldownUntil) return;
+        if (candidate == null || candidate == CurrentOwner) return;
 
+        SetOwner(candidate);
         _cooldownUntil = Time.time + _targetObjectSettings.captureCooldown;
     }
 
+    public void Detach()
+    {
+        CurrentOwner = null;
+        transform.SetParent(null);
+        _rigidbody.bodyType = RigidbodyType2D.Dynamic;
+    }
 
+
+    private void Awake()
+    {
+        Detach();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var carrier = GetComponentInParent<ICarrier>();
+        if (carrier != null) TryCapture(carrier);
+    }
+    
     private void SetOwner(ICarrier newOwner)
     {
         CurrentOwner = newOwner;
         transform.SetParent(newOwner.CarryPoint, worldPositionStays: false);
+        transform.localPosition = Vector3.zero;
+        _rigidbody.bodyType = RigidbodyType2D.Kinematic;
+        OnOwnerChanged?.Invoke();
     }
 }
